5e459b585c21291fbbad4dc9aa9243a87195adbd
Gogona828 <hw21a084@oecu.jp>
2026-06-10 12:36:43 +0900

    Delete: 不要なファイルの削除
    - 現状利用されていないファイルの削除
    - 怪力疑似遅延システムの削除

--- CombatGame/Assets/Script/Footsies/IRoundRuntimeState.cs
+++
@@ -1,7 +0,0 @@
-public interface IRoundRuntimeState
-{
-    int RoundTimerFrames { get; set; }
-    bool IsRoundOver { get; set; }
-    int WinnerPlayerId { get; set; }
-    uint RandomState { get; set; }
-}
